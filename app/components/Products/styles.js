e6de79a47ff32eacb700377b11eac3663db475f4
Gabriel Andreoni <gabriel.andreoni@hotmail.com>
2024-02-11 15:40:36 -0300

    Update styles.js

--- app/components/Products/styles.js
+++ app/components/Products/styles.js
@@ -17,11 +17,16 @@ export const ListProducts = styled.ul`
     display: flex;
     flex-wrap: wrap;
     justify-content: center;
-
     gap: 2em;
 
-
     list-style: none;
+
+    @media (max-width: 480px) {
+        flex-direction: column;
+        gap: 4em;
+
+        padding: 1em;
+    }
 
 `;
 
@@ -48,6 +53,10 @@ export const ProductItem = styled.li`
         box-shadow: rgba(0, 0, 0, 0.07) 0px 1px 1px, rgba(0, 0, 0, 0.07) 0px 2px 2px, rgba(0, 0, 0, 0.07) 0px 4px 4px, rgba(0, 0, 0, 0.07) 0px 8px 8px, rgba(0, 0, 0, 0.07) 0px 16px 16px;
     }
 
+    @media (max-width: 480px) {
+        width: 100%;
+    }
+
 `;
 
 export const ProductHeader = styled.div`
@@ -63,6 +72,10 @@ export const ProductImage = styled(Image)`
 
    &:hover {
         scale: 1.1;
+   }
+
+   @media (max-width: 480px) {
+        width: 100%;
    }
 `;
 
