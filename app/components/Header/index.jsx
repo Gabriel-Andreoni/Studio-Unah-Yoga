c044e2396f18a070d8c987b9e146c074ee413a12
Gabriel Andreoni <gabriel.andreoni@hotmail.com>
2024-05-12 12:25:10 -0300

    Criação do Menu

---
+++ app/components/Header/index.jsx
@@ -0,0 +1,21 @@
+import Image from "next/image";
+import './styles.css';
+
+export default function Header() {
+    return (
+        <header className="header-container">
+            <button className="button-mobile">
+                |
+            </button>            <nav className="nav-wrapper">
+                <ul className="menu-wrapper">
+                    <li className="menu-item">Home</li>
+                    <li className="menu-item">Sobre</li>
+                    <li className="menu-item">Serviços</li>
+                    <li className="menu-item">Produtos</li>
+                    <li className="menu-item">Blog</li>
+                    <li className="menu-item">Contato</li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
---
+++ app/page.js
@@ -0,0 +1,10 @@
+import Header from './components/Header';
+import './page.module.css';
+
+export default function Home() {
+  return (
+    <main className="">
+      <Header />
+    </main>
+  );
+}
