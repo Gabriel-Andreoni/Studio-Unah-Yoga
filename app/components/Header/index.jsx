ecf76471eb5d81871693506c602c1e6970657198
Gabriel Andreoni <gabriel.andreoni@hotmail.com>
2024-05-13 22:42:25 -0300

    update menu

--- app/components/Header/index.jsx
+++ app/components/Header/index.jsx
@@ -1,12 +1,21 @@
+"use client";
+
+import { useState } from "react";
 import Image from "next/image";
+import { RiMenu3Fill } from "react-icons/ri";
+import { IoMdClose } from "react-icons/io";
 import './styles.css';
 
 
 export default function Header() {
+    const [isActive, setIsActive] = useState(false);
     return (
-        <header className="header-container">
-            
+        <div>
+            <RiMenu3Fill className={`${isActive ? 'hide' : 'menu-icon'}`} onClick={() => setIsActive((prevState) => !prevState)} />
+             <header className={`${isActive ? 'header-container' : 'hide'}`}>
+            <IoMdClose className="menu-icon" onClick={() => setIsActive((prevState) => !prevState)} />
             <nav className="nav-wrapper">
+
                 <ul className="menu-wrapper">
                     <li className="menu-item">Home</li>
                     <li className="menu-item">Sobre</li>
@@ -17,5 +26,7 @@ export default function Header() {
                 </ul>
             </nav>
         </header>
+        </div>
+       
     )
 }
