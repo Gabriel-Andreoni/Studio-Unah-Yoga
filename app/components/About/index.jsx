d0d50732c802ebfe02575b190a7b71d46f1a2d75
Gabriel Andreoni <gabriel.andreoni@hotmail.com>
2024-05-27 13:02:33 -0300

    Criação da seção Sobre

---
+++ app/components/About/index.jsx
@@ -0,0 +1,15 @@
+import './styles.css';
+
+export default function About() {
+    return (
+        <section className="about-wrapper">
+            <div className="about-image">
+                
+            </div>
+            <div className="about-apresentation">
+                <h2>Onde você recuperar suas energias?</h2>
+                <p>O estudio Unah Yoga é um espaço acolhedor e tranquilo dedicado à prática do yoga. Oferecemos uma variedade de aulas, desde Hatha e Vinyasa até Yoga Restaurativa e Meditação Guiada, adequadas para todos os níveis de experiência. Nossos instrutores são certificados e comprometidos em proporcionar uma experiência de bem-estar holística. O ambiente do estúdio é sereno, com decoração minimalista, luz suave e aromas relaxantes, criando um refúgio perfeito para a mente e o corpo. Venha descobrir a harmonia e o equilíbrio no estudio Unah Yoga.</p>
+            </div>
+        </section>
+    )
+}
--- app/components/Hero/index.jsx
+++ app/components/Hero/index.jsx
@@ -6,7 +6,6 @@ import Header from "../Header";
 export default function Hero() {
     return (
         <div className="hero-wrapper">
-            <div className="overlay"></div>
             <Header />
 
             <div className="hero-container">
