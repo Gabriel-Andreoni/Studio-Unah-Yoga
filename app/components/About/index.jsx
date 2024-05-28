5daa98d01b372e26fe5dbd0910f454b2d0a9d8eb
Gabriel Andreoni <gabriel.andreoni@hotmail.com>
2024-05-28 12:59:49 -0300

    Alteração na seção Sobre

--- app/components/About/index.jsx
+++ app/components/About/index.jsx
@@ -6,12 +6,14 @@ import AboutImage from '/public/about-img.jpg'
 export default function About() {
     return (
         <section className="about-wrapper">
+            <div className="overlay"></div>
             <div className="about-image">
                 <Image src={AboutImage} alt="" width="500" height="500"/>
             </div>
             <div className="about-apresentation">
-                <h2>Onde você recuperar suas energias?</h2>
-                <p>O estudio Unah Yoga é um espaço acolhedor e tranquilo dedicado à prática do yoga. Oferecemos uma variedade de aulas, desde Hatha e Vinyasa até Yoga Restaurativa e Meditação Guiada, adequadas para todos os níveis de experiência. Nossos instrutores são certificados e comprometidos em proporcionar uma experiência de bem-estar holística. O ambiente do estúdio é sereno, com decoração minimalista, luz suave e aromas relaxantes, criando um refúgio perfeito para a mente e o corpo. Venha descobrir a harmonia e o equilíbrio no estudio Unah Yoga.</p>
+                <h2>Onde você recupera suas energias?</h2>
+                <p>O estudio Unah Yoga é um espaço acolhedor e tranquilo dedicado à prática do yoga. Oferecemos uma variedade de aulas, desde Hatha e Vinyasa até Yoga Restaurativa e Meditação Guiada, adequadas para todos os níveis de experiência.<br /><br /> Nossos instrutores são certificados e comprometidos em proporcionar uma experiência de bem-estar holística. O ambiente do estúdio é sereno, com decoração minimalista, luz suave e aromas relaxantes, criando um refúgio perfeito para a mente e o corpo. Venha descobrir a harmonia e o equilíbrio no estudio Unah Yoga.</p>
+                <button>Entre em contato</button>
             </div>
         </section>
     )
