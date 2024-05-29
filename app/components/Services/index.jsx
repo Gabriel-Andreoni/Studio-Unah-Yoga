f7ff70771cb4e4c3b337b3de26a331dbaa9087fe
Gabriel Andreoni <gabriel.andreoni@hotmail.com>
2024-05-29 12:46:21 -0300

    Criação da Seção de Serviços

---
+++ app/components/Services/index.jsx
@@ -0,0 +1,23 @@
+import './styles.css';
+
+const prodocuts = [
+    {
+        id: 1,
+        name: '',
+        description: '',
+        image: '',
+        
+    }
+]
+export default function Services() {
+    return (
+        <section className="services-wrapper">
+            <div className="services-header">
+                <h1>Produtos</h1>
+            </div>
+            <div className="services-content">
+
+            </div>
+        </section>        
+    )
+}
