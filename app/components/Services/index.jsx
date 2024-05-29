15817dcab4dfc283125b4c48654541934856bfe9
Gabriel Andreoni <gabriel.andreoni@hotmail.com>
2024-05-29 13:09:30 -0300

    update

--- app/components/Services/index.jsx
+++ app/components/Services/index.jsx
@@ -3,12 +3,39 @@ import './styles.css';
 const prodocuts = [
     {
         id: 1,
-        name: '',
-        description: '',
+        title: 'Atividade?',
+        subtitle: 'Yoga Ativo',
+        description: () => {
+            return (
+                <>
+                    <h3>Para os que buscam se desafiar</h3>
+                    <p>Proposta de meditação em movimento, unindo posturas e respiração <br />Aprimora o corpo físico, a capacidade respiratória e senso de autoconfiança.</p>
+                </>
+            )
+        },
         image: '',
-        
+    },
+
+    {
+        id: 2,
+        title: 'Presença?',
+        subtitle: 'Yoga Presente',
+        description: () => {
+            return (
+                <>
+                    <h3>Para os que buscam se desafiar</h3>
+                    <p>Proposta de meditação em movimento, unindo posturas e respiração <br />Aprimora o corpo físico, a capacidade respiratória e senso de autoconfiança.</p>
+                </>
+            )
+        }
     }
 ]
+
+const loopedProducts = prodocuts.map((product) => {
+    return (
+        <li key={product.id}>{product.title}</li>
+    )
+})
 export default function Services() {
     return (
         <section className="services-wrapper">
@@ -16,8 +43,10 @@ export default function Services() {
                 <h1>Produtos</h1>
             </div>
             <div className="services-content">
-
+                <ul>
+                    {loopedProducts}
+                </ul>
             </div>
-        </section>        
+        </section>
     )
 }
